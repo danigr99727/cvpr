c80a28e3bf4d60d7d90a6a854655441e9bd4b544
ed13santi <37627980+ed13santi@users.noreply.github.com>
2021-02-04 20:48:22 +0000

    initial commit Edo - add my files

---
+++ Matlab/homography.m
@@ -0,0 +1,20 @@
+I1 = rgb2gray(imread('20210204_160141.jpg'));
+I2 = rgb2gray(imread('20210204_160143.jpg'));
+
+points1 = detectHarrisFeatures(I1);
+points2 = detectHarrisFeatures(I2);
+%points1 = detectSURFFeatures(I1);
+%points2 = detectSURFFeatures(I2);
+
+[features1,valid_points1] = extractFeatures(I1,points1);
+[features2,valid_points2] = extractFeatures(I2,points2);
+
+indexPairs = matchFeatures(features1,features2);
+
+matchedPoints1 = valid_points1(indexPairs(:,1),:);
+matchedPoints2 = valid_points2(indexPairs(:,2),:);
+
+%figure; showMatchedFeatures(I1,I2,matchedPoints1,matchedPoints2);
+
+x2 = matchedPoints2.Location;
+x1 = matchedPoints1.Location;
